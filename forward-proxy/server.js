c407fe2e7cf7348d9993fc9d2315666d8d47388b
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-10 10:43:43 +0200

    forwarding to target

--- forward-proxy/server.js
+++ forward-proxy/server.js
@@ -1,10 +1,22 @@
 const http = require("http")
+const httpProxy = require('http-proxy')
 require('dotenv').config()
 console.log(process.env.ENV)
+const proxy = httpProxy.createProxyServer({})
+
+const targets = process.env.TARGETS
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.end('<h1>Hello World!</h1>')
+    if (req.url == "/") {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.end('<h1>Hello World!</h1>')
+    } else {
+        const url = targets[0]
+        proxy.web(req, res, { target: url }, (err) => {
+            res.writeHead(res.statusCode, { 'content-type': 'text/html' })
+            res.end()
+        })
+    }
 })
 
 
