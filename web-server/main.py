fd66329304a595362194ec6fef881fdf06901180
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-10 09:48:58 +0200

    ref: support dev/docker envs

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,14 @@
+import sys
+
 import uvicorn
 
 from src.app import create_app
 
-if __name__ == "__main__":
-    app = create_app()
-    uvicorn.run(app=app, port=3000)
+n = len(sys.argv)
+env = sys.argv[1]
+host = sys.argv[2]
+environments = ['docker', 'dev']
+
+if env in environments:
+    app = create_app(env=env)
+    uvicorn.run(app=app, host=host, port=3000)
