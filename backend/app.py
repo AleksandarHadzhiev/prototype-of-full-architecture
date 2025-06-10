38f801c87ce8e1bb006a21d8309f9305398f11aa
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-10 16:25:42 +0200

    retun message as in issue

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+import sys
+import uvicorn
+
+from src.main import create_app
+
+n = len(sys.argv)
+env = sys.argv[1]
+host = sys.argv[2]
+environments = ['docker', 'dev']
+
+if env in environments:
+    app = create_app(env=env)
+    uvicorn.run(app=app, host=host,port=8080)
