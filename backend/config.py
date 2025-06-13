8b8b87d68347d748a7b205614a88d31cf7fff8b8
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-13 14:59:02 +0200

    database connection values

--- backend/config.py
+++ backend/config.py
@@ -3,11 +3,25 @@ class Config:
     BASE_URL = ""
     BACKEND_URL = ""
     ALLOWED_ORIGINS = []
+    POSTGRES_HOST = ""
+    POSTGRES_DB = ""
+    POSTGRES_USER = ""
+    POSTGRES_PASSWORD = ""
+    POSTGRES_MASTER_PORT = ""
+    POSTGRES_SLAVE_ONE_PORT = ""
+    POSTGRES_SLAVE_TWO_PORT = ""
 
 class DevConfig(Config):
     ENV = "dev"
     BASE_URL = "http://localhost:8080"
     ALLOWED_ORIGINS = "http://localhost:8000"
+    POSTGRES_HOST = "127.0.0.1"
+    POSTGRES_DB = "todos"
+    POSTGRES_USER = "postgres"
+    POSTGRES_PASSWORD = "postgres"
+    POSTGRES_MASTER_PORT = "5432"
+    POSTGRES_SLAVE_ONE_PORT = "5555"
+    POSTGRES_SLAVE_TWO_PORT = "5556"
 
 class DockerConfig(Config):
     ENV = "docker"
