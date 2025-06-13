4a57510302966b2bd202c71d795d9569db161a66
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-13 10:22:15 +0200

    connected to port:8000

--- reverse-proxy/src/app.py
+++ reverse-proxy/src/app.py
@@ -1,1 +1,12 @@
- 
+import json
+from fastapi import FastAPI, Request, Response, status
+
+def create_app(env="dev"):
+    print(env)
+    app = FastAPI()
+
+    @app.get("/")
+    def index(req: Request) -> Response:
+        return Response(content=json.dumps({"message": "Hello, World!"}), status_code=status.HTTP_200_OK)
+
+    return app
