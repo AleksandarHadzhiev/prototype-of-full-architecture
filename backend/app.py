62d62e7edb703a264f743033048b515560fdd8d8
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-13 11:44:15 +0200

    load balancer integration

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,11 @@ n = len(sys.argv)
 env = sys.argv[1]
 host = sys.argv[2]
 environments = ['docker', 'dev']
+port = 8080
+
+if sys.argv[3]:
+    port = 8081
 
 if env in environments:
     app = create_app(env=env)
-    uvicorn.run(app=app, host=host,port=8080)
+    uvicorn.run(app=app, host=host,port=port)
--- reverse-proxy/src/app.py
+++ reverse-proxy/src/app.py
@@ -1,12 +1,34 @@
 import json
+import requests
 from fastapi import FastAPI, Request, Response, status
+from config import load_config
+
+class LoadBalancer:
+    def __init__(self, servers: list[str]):
+        self.servers = servers
+        self.index =0
+
+    def get_server(self) -> str:
+        server = ""
+        if self.index < len(self.servers) -1:
+            server = self.servers[self.index]
+            self.index += 1
+        else:
+            server = self.servers[self.index]
+            self.index = 0
+        return server
 
 def create_app(env="dev"):
-    print(env)
+    config = load_config(env=env)
+    targets: list[str] = config.TARGETS
+    balancer = LoadBalancer(servers=targets)
+
     app = FastAPI()
-
     @app.get("/")
-    def index(req: Request) -> Response:
-        return Response(content=json.dumps({"message": "Hello, World!"}), status_code=status.HTTP_200_OK)
-
+    def route(req: Request) -> Response:
+        free_server = balancer.get_server()
+        print(free_server)
+        response = requests.get(f"{free_server}/aleks")
+        body = response.json()
+        return Response(content=json.dumps(body), status_code=status.HTTP_200_OK)
     return app
