2883063c85e0223b5ae770e1785d18e2a91c7130
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-13 15:18:30 +0200

    ref docker compose for the correct db init

--- backend/init.sql
+++ backend/init.sql
@@ -1,9 +1,8 @@
- CREATE TABLE IF NOT EXISTS todos 
+CREATE TABLE IF NOT EXISTS todos 
                 (id SERIAL, 
                 title VARCHAR(255) NOT NULL,
-                content VARCHAR() NOT NULL, 
+                content TEXT NOT NULL, 
                 date_created VARCHAR(25) NOT NULL,
-                date_completed VARCHAR(25) DEFAULT NULL,
-                date_to_complete VARCHAR(25) DEFAULT NULL,
-                status VARCHAR() DEFAULT 'pending');
-
+                date_completed VARCHAR(25),
+                date_to_complete VARCHAR(25),
+                status VARCHAR(15) DEFAULT 'pending');
